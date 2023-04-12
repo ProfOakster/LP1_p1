45da114d711935571d42a1dd43c040cdb33ff765
TomasCarvalho <2606tom@gmail.com>
2023-04-12 17:33:12 +0100

    Add placeholder text in main

--- LampPuzzle/Program.cs
+++ LampPuzzle/Program.cs
@@ -6,7 +6,29 @@ namespace LampPuzzle
     {
         static void Main(string[] args)
         {
+            
+            int presses_left = 6;
+
             Console.WriteLine("");
+            Console.WriteLine("LAMP PUZZLE");
+            Console.WriteLine("First button toggles first lamp");
+            Console.WriteLine("others swap state of its lamp and its previous one");
+
+            Console.WriteLine($"Light all lamps in {presses_left} moves!");
+            
+            while (presses_left>0)
+            {
+                
+            Console.WriteLine("");
+
+
+
+
+
+
+            }
+
+
         }
     }
 }
