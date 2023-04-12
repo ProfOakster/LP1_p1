9c85b8b3027f6d996da5b539016587e2c4931d21
joaofrazaocorreia <joaopfrazaocorreia@gmail.com>
2023-04-12 16:59:26 +0100

    Add method ButtonPress with three buttons

--- LampPuzzle/Program.cs
+++ LampPuzzle/Program.cs
@@ -8,5 +8,48 @@ namespace LampPuzzle
         {
             Console.WriteLine("");
         }
+
+        static LampStates ButtonPress(int button)
+        {
+            LampStates lamps = 0;
+
+            if (button == 1)
+            {
+                lamps ^= LampStates.lamp1;
+                Console.WriteLine("The first lamp turned on!");
+            }
+
+            else if (button == 2)
+            {
+                if ((lamps & LampStates.lamp1) != (lamps & LampStates.lamp1))
+                {
+                    lamps ^= LampStates.lamp1;
+                    lamps ^= LampStates.lamp2;
+                    Console.WriteLine("Lamps 1 and 2 switched states!");
+                }
+
+                else
+                {
+                    Console.WriteLine("Nothing happened!");
+                }
+            }
+            
+            else if (button == 3)
+            {
+                if ((lamps & LampStates.lamp1) != (lamps & LampStates.lamp1))
+                {
+                    lamps ^= LampStates.lamp2;
+                    lamps ^= LampStates.lamp3;
+                    Console.WriteLine("Lamps 2 and 3 switched states!");
+                }
+
+                else
+                {
+                    Console.WriteLine("Nothing happened!");
+                }
+            }
+
+            return lamps;
+        }
     }
 }
