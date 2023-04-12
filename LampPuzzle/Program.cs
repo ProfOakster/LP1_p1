874a591b320889dda11262f03bf4dd7625ce5175
TomasCarvalho <2606tom@gmail.com>
2023-04-12 18:40:34 +0100

    Begin implementing main loop

--- LampPuzzle/Program.cs
+++ LampPuzzle/Program.cs
@@ -6,26 +6,28 @@ namespace LampPuzzle
     {
         static void Main(string[] args)
         {
-            
-            int presses_left = 6;
 
+            int presses_left = 6;
+            LampStates lamps = 0;
             Console.WriteLine("");
             Console.WriteLine("LAMP PUZZLE");
             Console.WriteLine("First button toggles first lamp");
             Console.WriteLine("others swap state of its lamp and its previous one");
 
             Console.WriteLine($"Light all lamps in {presses_left} moves!");
-            
-            while (presses_left>0)
+
+            while (presses_left > 0)
             {
-                
-            Console.WriteLine("");
+
+                Console.WriteLine($"You have {presses_left} turns left");
+                Console.WriteLine("");
 
 
 
 
 
 
+                presses_left -= 1;
             }
 
 
