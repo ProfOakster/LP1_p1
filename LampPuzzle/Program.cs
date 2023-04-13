c2bc1a14ea297786ae6a64bead11bae6be919233
TomasCarvalho <2606tom@gmail.com>
2023-04-13 20:15:24 +0100

    Add code documentation; Improve user instructions' clarity

--- LampPuzzle/Program.cs
+++ LampPuzzle/Program.cs
@@ -7,37 +7,59 @@ namespace LampPuzzle
     /// </summary>
     class Program
     {
+        /// <summary>
+        /// Implementation of the Lamp Puzzle with 3 lamps and 3 buttons
+        /// </summary>
+        /// <param name="args">Unused.</param>
         static void Main(string[] args)
         {
-
+            //Enables writing unicode to command line
             Console.OutputEncoding = Encoding.UTF8;
 
+            //Declaration of basic variables
             LampStates lamps = 0;
             int lamps_num = Enum.GetNames(typeof(LampStates)).Length;
             int buttons_num = lamps_num;
             int presses_left = 6;
 
-            Console.WriteLine("");
-            Console.WriteLine("LAMP PUZZLE");
-            Console.WriteLine("First button toggles first lamp");
-            Console.WriteLine("others swap state of its lamp and its previous one");
+            //Player instructions
+            Console.WriteLine("--------------------------");
+            Console.WriteLine("{0,18}", "LAMP PUZZLE");
+            Console.WriteLine("--------------------------");
 
-            Console.WriteLine($"Light all lamps in {presses_left} moves!");
-            DisplayLamps(lamps, lamps_num);
+            Console.WriteLine("There are {0} lamps and {1} buttons;",
+             lamps_num, buttons_num);
+            Console.WriteLine("Pressing the first button toggles the first " +
+            "lamp between on and off;");
+            Console.WriteLine("The others swap the states between its " +
+            "corresponding lamp and its previous one");
+            Console.WriteLine("(Input a number to press that button)");
+            Console.WriteLine(@"(Input 'q' to Quit)");
+            Console.WriteLine($"Try to light all lamps in {presses_left} moves!");
+            Console.WriteLine("--------------------------");
 
+            //Game loop; Each cycle corresponds to 1 turn; Ends when out of turns
             while (presses_left > 0)
             {
+                DisplayLamps(lamps, lamps_num);
 
-                Console.WriteLine($"You have {presses_left} turns left");
+                string message = presses_left switch
+                {
+                    1 => "You have 1 turn left.",
+                    _ => String.Format($"You have {presses_left} turns left."),
+                };
+                Console.WriteLine(message);
 
-
-
-
+                //User input cycle; Keeps asking until a valid input is received
+                //Valid inputs are real numbers between 1 and the number of
+                //buttons, or "q" to quit
                 bool invalid_input = true;
                 int selection = 0;
                 do
                 {
                     string input = Console.ReadLine();
+                    //If input can be converted to an int between 1 and 
+                    //buttons_num
                     if (int.TryParse(input, out int int_input) &&
                     (0 < int_input) && (int_input <= buttons_num))
                     {
@@ -54,37 +76,48 @@ namespace LampPuzzle
 
                 } while (invalid_input);
 
+                //When player wants to Quit
+                //Skips straight to Game Over
                 if (selection == -1)
                 {
                     Console.WriteLine("You give up.");
                     break;
                 }
 
+                //Alters lamps according to button pressed
                 lamps = ButtonPress(selection, lamps);
 
-                DisplayLamps(lamps, lamps_num);
 
+                Console.WriteLine("--------------------------");
                 presses_left -= 1;
 
+                //Checks if player won with moves to spare
                 if (CheckWin(lamps))
                     break;
 
             }
 
+            //Displays victory or loss message
             if (CheckWin(lamps))
             {
-                Console.WriteLine($"You won with {presses_left} turns left!");
+                string message = presses_left switch
+                {
+                    1 => "You won with 1 turn left!",
+                    _ => String.Format($"You won with {presses_left} turns left!"),
+                };
+                Console.WriteLine(message);
             }
             else
             {
-                Console.WriteLine("No turns left");
+                if (presses_left == 0)
+                    Console.WriteLine("No turns left");
                 Console.WriteLine("GAME OVER");
             }
 
 
         }
         /// <summary>
-        /// Recieves the number of the button pressed and the variable 
+        /// Receives the number of the button pressed and the variable 
         /// containing the lamp states, executing the button's action and
         /// updating the variable.
         /// </summary>
@@ -115,7 +148,7 @@ namespace LampPuzzle
                     ///Toggles both lamps and prints feedback to the console.
                     lamps ^= (LampStates)(1 << (button - 1));
                     lamps ^= (LampStates)(1 << button);
-                    Console.WriteLine($"Lamps {button} and {button + 1}"+
+                    Console.WriteLine($"Lamps {button} and {button + 1}" +
                                       " switched states!");
                 }
 
@@ -134,7 +167,7 @@ namespace LampPuzzle
         /// Recieves the variable containing the lamp states and checks if
         /// any of them is turned off.
         /// </summary>
-        /// <param name="lamps"></param>
+        /// <param name="lamps">The variable containing the lamp states</param>
         /// <returns>True if all lamps are on;
         /// False if at least one lamp is off.</returns>
         static bool CheckWin(LampStates lamps)
@@ -161,11 +194,20 @@ namespace LampPuzzle
             return win;
         }
 
+        /// <summary>
+        /// Displays the current state of all the lamps on the command line
+        /// </summary>
+        /// <param name="lamps">Variable containing the lamp states</param>
+        /// <param name="lamps_num">Variable containing the number of lamps</param>
         static void DisplayLamps(LampStates lamps, int lamps_num)
         {
+            //Stores the console foreground's current color 
+            //and changes it to yellow
             ConsoleColor reset = Console.ForegroundColor;
             Console.ForegroundColor = ConsoleColor.Yellow;
 
+            //Displays the lamp number and a corresponding unicode character 
+            //according to its state
             for (int i = 1; i <= lamps_num; i++)
             {
                 char lamp;
@@ -175,8 +217,8 @@ namespace LampPuzzle
                     lamp = '\u25CB';
                 Console.WriteLine("Lamp{0,1} {1,-3}{2}", i, "-", lamp);
             }
+            //Resets the console foreground's color
             Console.ForegroundColor = reset;
-
         }
 
 
